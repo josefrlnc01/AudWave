9ccccdeb0a51c04fd25d92af429926694ead6340
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-12 16:58:43 +0100

    adición de campo title en modelo de vídeos del dispositivo del usuario

--- backend/modules/file/file.model.ts
+++ backend/modules/file/file.model.ts
@@ -1,12 +1,17 @@
 import mongoose, {Document, Types, Schema} from "mongoose";
 
 interface IFileStored extends Document {
+    title: string,
     text: string,
     translated: string | null
     user: Types.ObjectId
 }
 
 const fileSchema: Schema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
     text: {
         type: String,
         required: true
