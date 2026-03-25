67fb75724f017ae0eba530e3858cbea435c36255
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-25 09:20:13 +0100

    mejora de función de formateo de timestamps

--- frontend/src/shared/utils/minutes.ts
+++ frontend/src/shared/utils/minutes.ts
@@ -9,5 +9,7 @@ export function formatTime(seconds: number) {
         const hrs = Math.floor(seconds / 3600)
         const mins = Math.floor((seconds % 3600) / 60)
         const secs = Math.floor(seconds % 60)
-        return `[${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}]`
+        return hrs >= 1 
+        ? `[${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}]` 
+        : `[${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}]`
     }
