6734190fab4220e512dfe7916151399f19a75f53
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-29 19:37:54 +0200

    cambios visuales

--- frontend/src/pages/landing/components/Header.tsx
+++ frontend/src/pages/landing/components/Header.tsx
@@ -6,13 +6,13 @@ export default function Header() {
     <header className='w-full bg-slate-950 border-b border-slate-800/60 px-6 py-4 shrink-0'>
       <div className='max-w-5xl mx-auto flex items-center justify-between'>
         <div className='flex items-center justify-center'>
-        <h1 className="font-bold text-4xl text-white">Aud<span className="text-blue-600/80">Wave</span></h1>
+        <h1 className="font-bold text-lg md:text-2xl lg:text-4xl text-white">Aud<span className="text-blue-600/80">Wave</span></h1>
         </div>
         
         <div className=' flex justify-start'>
           <button
             onClick={() => navigate('/auth/login')}
-            className='pt-2 pb-2 pr-4 pl-4 bg-blue-600 cursor-pointer hover:bg-blue-700 rounded-xl font-bold text-lg shadow-xl transition-colors duration-200 ease-in'
+            className='pt-2 pb-2 pr-4 pl-4 bg-blue-600 cursor-pointer hover:bg-blue-700 rounded-xl font-bold text-sm md:text-md lg:text-lg shadow-xl transition-colors duration-200 ease-in'
           >Iniciar sesión</button>
         </div>
       </div>
