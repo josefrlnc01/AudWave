d4a876ea182e4e563f67f1cd889735ee9f2148e7
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-30 17:07:43 +0200

    cambio hovers

--- frontend/src/features/saveds/components/SavedsList.tsx
+++ frontend/src/features/saveds/components/SavedsList.tsx
@@ -44,20 +44,20 @@ export default function SavedsList() {
                 value={inputValue}
                 onChange={handleInputValue}
                 placeholder="Buscar archivo..."
-                className={`w-full pl-9 pr-3 py-3 ${theme === 'dark' ? 'bg-slate-800 border border-slate-700 text-gray-200' : 'bg-slate-200 text-slate-900'} rounded-lg text-sm  placeholder-slate-400 focus:outline-none focus:border-blue-500`}
+                className={`w-full pl-3 pr-3 py-3 ${theme === 'dark' ? 'bg-slate-800 border border-slate-700 text-gray-200' : 'bg-slate-200 text-slate-900'} rounded-lg text-sm  placeholder-slate-400 focus:outline-none focus:border-blue-500`}
               />
               
               {hasSaveds && filtereds.length === 0 && inputValue.trim() === '' &&
                 <>
         
                   {files.map(file => (
-                      <Link to={`/saveds/${file.fileId}`} className={`${theme === 'dark' ? 'bg-slate-800/70  hover:bg-slate-700/90' : 'bg-slate-300 hover:bg-slate-400/80'} p-4 md:p-6 gap-2 rounded-md flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer`} key={file._id}>
+                      <Link to={`/saveds/${file.fileId}`} className={`${theme === 'dark' ? 'bg-slate-800/70  hover:bg-slate-700/90' : 'bg-slate-300 hover:bg-slate-400/80'} p-4 md:p-6 gap-2 rounded-md flex justify-between items-center hover:scale-[1.02] transition-all duration-200 ease cursor-pointer`} key={file._id}>
                         <h4 className='wrap-break-word text-start text-sm md:text-md'>{file.title}</h4>
                         <span className='text-white min-w-20 max-w-xs text-xs md:text-sm flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-2 pr-2 rounded-2xl'>{file.duration}</span>
                       </Link>
                   ))}
                   {youtubeFiles.map(file => (
-                      <Link to={`/saveds/${file.fileId}`} className={`${theme === 'dark' ? 'bg-slate-800/70  hover:bg-slate-700/90' : 'bg-slate-300 hover:bg-slate-200'} p-4 md:p-6 gap-2 rounded-md flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer`} key={file._id}>
+                      <Link to={`/saveds/${file.fileId}`} className={`${theme === 'dark' ? 'bg-slate-800/70  hover:bg-slate-700/90' : 'bg-slate-300 hover:bg-slate-200'} p-4 md:p-6 gap-2 rounded-md flex justify-between items-center hover:scale-[1.02] transition-all duration-200 ease cursor-pointer`} key={file._id}>
                         <h4 className='wrap-break-word text-start text-sm md:text-md'>{file.title}</h4>
                         <span className='text-white min-w-20 max-w-xs text-xs md:text-sm flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-2 pr-2 rounded-2xl'>{file.duration}</span>
                       </Link>
@@ -70,7 +70,7 @@ export default function SavedsList() {
                   <div className="w-full mb-6">
                   </div>
                   {filtereds.map(file => (
-                      <Link to={`/saveds/${file.fileId}`} className={`${theme === 'dark' ? 'bg-slate-800/70  hover:bg-slate-700/90' : 'bg-slate-300 hover:bg-slate-200'} p-4 md:p-6 gap-2 rounded-md flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer`} key={file._id}>
+                      <Link to={`/saveds/${file.fileId}`} className={`${theme === 'dark' ? 'bg-slate-800/70  hover:bg-slate-700/90' : 'bg-slate-300 hover:bg-slate-200'} p-4 md:p-6 gap-2 rounded-md flex justify-between items-center hover:scale-[1.02] transition-all duration-200 ease cursor-pointer`} key={file._id}>
                         <h4 className={`${theme === 'dark' ? 'text-white' : 'text-slate-900'} max-w-xs wrap-break-word text-start text-sm md:text-md`}>{file.title}</h4>
                         <span className={`text-white min-w-20 text-xs md:text-sm flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-2 pr-2 rounded-2xl`}>{file.duration}</span>
                       </Link>
