a444050b6b1c2593dff864d6a1e699a5bf68b31e
josefrlnc01 <josefrlnc01@gmail.com>
2026-04-21 17:26:52 +0200

    cambio condicional conexión de bullmq

--- backend/config/queue.ts
+++ backend/config/queue.ts
@@ -3,7 +3,7 @@ import {Queue} from 'bullmq'
 const isProd = process.env.NODE_ENV === 'production'
 
 export const connection = {
-    host: isProd ? process.env.REDIS_HOST?.trim() : '127.0.0.1',
+    host: !isProd ? process.env.REDIS_HOST?.trim() : '127.0.0.1',
     port: Number(process.env.REDIS_PORT ?? 6379),
     maxRetriesPerRequest: null
 }
