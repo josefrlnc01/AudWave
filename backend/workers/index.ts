2b99cd9234d07f2906eb218d917f37d9cea57568
josefrlnc01 <josefrlnc01@gmail.com>
2026-04-23 16:18:52 +0200

    cambio de configuracion dotenv

--- backend/workers/index.ts
+++ backend/workers/index.ts
@@ -5,7 +5,7 @@ import { dirname, resolve } from 'node:path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-dotenv.config({path: resolve(__dirname, '../../.env')})
+dotenv.config({path: resolve(__dirname, '../.env')})
 await import( './transcription.worker.js')
 
 console.log('workers iniciados')
