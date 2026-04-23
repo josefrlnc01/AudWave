10a760257f5ed484c3777d04d1b8591636d21ff5
josefrlnc01 <josefrlnc01@gmail.com>
2026-04-23 16:36:16 +0200

    conexión a mongo db en worker

--- backend/workers/index.ts
+++ backend/workers/index.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import dotenv from 'dotenv'
 import { dirname, resolve } from 'node:path'
 import dns from 'node:dns'
+import { connectToDb } from '../config/db.js'
 dns.setDefaultResultOrder('ipv4first')
 dns.setServers(['8.8.8.8', '8.8.4.4'])
 const __filename = fileURLToPath(import.meta.url)
@@ -12,6 +13,8 @@ console.log('ENV PATH:', envPath)
 console.log('DOTENV RESULT:', result.error ?? 'OK')
 console.log('NODE_ENV:', process.env.NODE_ENV)
 console.log('REDIS_HOST:', process.env.REDIS_HOST)
+
+await connectToDb()
 await import( './transcription.worker.js')
 
 
